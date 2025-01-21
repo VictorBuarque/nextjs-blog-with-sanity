d2bee53a66574ac6094122d87a1dbcce0bb1fdd5
Victor Buarque <vcgabriel38@gmail.com>
2025-01-20 23:58:20 -0300

    refactor: page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,41 @@
-import Image from "next/image";
-import { client } from "@/sanity/lib/client";
-import Link from "next/link";
+import { client } from "@/src/app/lib/sanity/lib/client"
+import BlogPostCard from "./components/card/Card"
+import { BlogPost } from "./types/BlogPost"
 
-const options = { next: { revalidate: 60 } }; // Revalidate cache every 60 seconds
+
+const options = { next: { revalidate: 60 } } // Revalidate cache every 60 seconds
 
 // GROQ Query for fetching blog posts
-const EVENTS_QUERY = `*[_type == "blog" && defined(slug.current)]{
+const BLOG_POSTS_QUERY = `*[_type == "blog" && defined(slug.current)] | order(publishedAt desc) {
   _id,
   title,
-  description,
-  date,
-  slug
-}|order(date desc)`;
+  "slug": slug.current,
+  mainImage,
+  excerpt,
+  author->{
+    _id,
+    name,
+    "slug": slug.current,
+    image
+  },
+  publishedAt
+}`
 
 export default async function Home() {
-  const data = await client.fetch(EVENTS_QUERY, {}, options);
+  const blogPosts: BlogPost[] = await client.fetch(BLOG_POSTS_QUERY, {}, options)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
-      <h1 className="text-4xl font-bold -tracking-tighter">TESTE</h1>
-      <ul className="grid grid-cols-1 gap-12 lg:grid-cols-2">
-        {data.map((item: any) => (
-          <li className="bg-white p-4 rounded-lg" key={item._id}>
-            <Link href={`/events/${item.slug.current}`}>
-              <h2 className="text-xl font-semibold text-black">{item.title}</h2>
-              {item.date && (
-                <p className="text-sm text-gray-600 dark:text-gray-400">
-                  {new Date(item.date).toLocaleDateString()}
-                </p>
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <main className="min-h-screen bg-background text-foreground">
+      <div className="max-w-7xl mx-auto px-4 py-12">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-20">
+          {blogPosts.map((post, index) => (
+            <div key={post._id} className={index === 0 ? "md:col-span-2 lg:col-span-3" : ""}>
+              <BlogPostCard post={post} isFirst={index === 0} />
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
-  );
+  )
 }
+
