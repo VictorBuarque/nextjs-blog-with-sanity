e9d087cd1c708b68977d1d55a920a01e99ebbb04
Victor Buarque <vcgabriel38@gmail.com>
2025-01-20 23:56:25 -0300

    chore: move files

--- sanity/schema.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { blogType } from './schemaTypes/blog'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blogType],
-}
